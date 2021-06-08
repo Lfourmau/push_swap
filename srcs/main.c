a7638869d25302e8a7694690e4b085cc87c02fe7
Loic Fourmaux <lfourmau@z2r8p8.42lyon.fr>
2021-06-08 10:22:29 +0200

    leaks fixed with ./ but all numbers leak with -5645 456456 454 45 456457

--- srcs/main.c
+++ srcs/main.c
@@ -6,6 +6,21 @@ int	error(void)
 	return (1);
 }
 
+int free_exit(t_stacks *stacks)
+{
+	free(stacks->stack_a);
+	free(stacks->stack_b);
+	free(stacks);
+	return (0);
+}
+
+void	free_splits(char **chain, int i)
+{
+	while (--i >= 0)
+		free(chain[i]);
+	free(chain);
+}
+
 int	main(int argc, char **argv)
 {
 	t_stacks	*stacks;
@@ -13,4 +28,5 @@ int	main(int argc, char **argv)
 	stacks = malloc(sizeof(t_stacks *));
 	if (parsing(argc, argv, stacks))
 		return (error());
+	free_exit(stacks);
 }
