36c3118b1852fe1ff38ed550487eb09ac142b794
Loic Fourmaux <lfourmau@z2r8p8.42lyon.fr>
2021-06-08 14:36:47 +0200

    need to fix the copy of A in B stack.

--- srcs/main.c
+++ srcs/main.c
@@ -13,11 +13,20 @@ int	main(int argc, char **argv)
 	if (parsing(argc, argv, &stacks))
 		return (error());
 
-	int i = 0;
-	while (i <= stacks.max_index)
-	{
-		printf("%d\n", stacks.stack_a[i]);
-		i++;
-	}
+	// int i = 0;
+	// while (i <= stacks.max_index)
+	// {
+	// 	printf("%d\n", stacks.stack_a[i]);
+	// 	i++;
+	// }
+	// sa(&stacks);
+	// printf("//////////////////////////////\n");
+	// i = 0;
+	// ft_memcpy(stacks.stack_b, stacks.stack_a, stacks.max_index + 2 * 16);
+	// while (i <= stacks.max_index)
+	// {
+	// 	printf("%d\n", stacks.stack_b[i]);
+	// 	i++;
+	// }
 	// free_exit(stacks); necessaire pour regler les 3 derniers leaks, mais dit qu'on modifie un evaleur apres l'avoir free.
 }
