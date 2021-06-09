5c5cc4a0035ea501a6e4c01de5fd1a1d1292fdef
Loic Fourmaux <lfourmau@z2r8p8.42lyon.fr>
2021-06-09 15:20:41 +0200

    clean main. nothing else

--- srcs/main.c
+++ srcs/main.c
@@ -13,36 +13,5 @@ int	main(int argc, char **argv)
 
 	if (parsing(argc, argv, &stacks))
 		return (error());
-	
-	i = 0;
-	while (i <= stacks.stack_a.max_index)
-	{
-		printf("%d\n", stacks.stack_a.stack[i]);
-		i++;
-	}
-	i = 0;
-	stack_a_to_b(&stacks);
-	while (i <= stacks.stack_b.max_index)
-	{
-		printf("{%d}\n", stacks.stack_b.stack[i]);
-		i++;
-	}
-
-	printf("------\n");
-	rrr(&stacks);
-	printf("------\n");
-
-	i = 0;
-	while (i <= stacks.stack_a.max_index)
-	{
-		printf("%d\n", stacks.stack_a.stack[i]);
-		i++;
-	}
-	i = 0;
-	while (i <= stacks.stack_b.max_index)
-	{
-		printf("{%d}\n", stacks.stack_b.stack[i]);
-		i++;
-	}
 	// free_exit(stacks); necessaire pour regler les 3 derniers leaks, mais dit qu'on modifie un evaleur apres l'avoir free.
 }
